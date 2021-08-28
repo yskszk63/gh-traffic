c4e60924b600622af0e6062b252f8c71153dcb10
yskszk63 <yskszk63@gmail.com>
2021-08-28 16:16:53 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# gh-traffic
+
+gh extension for GitHub repositories traffic viewer.
+
+## Install
+
+```
+gh extension install yskszk63/traffic
+```
+
+## Usage
+
+TODO screen shot.
+
+```
+gh traffic
+```
+
+## License
+
+MIT
+
+## Author
+
+yskszk63
---
+++ mod.ts
@@ -0,0 +1,74 @@
+import { readLines } from "https://deno.land/std@0.106.0/io/mod.ts";
+import * as tty from "https://deno.land/x/tty@0.1.2/mod.ts";
+
+const isatty = Deno.isatty(Deno.stdout.rid);
+
+const list: {name: string, count: number, uniques: number}[] = [];
+const print = () => {
+    list.sort((l, r) => {
+        const x = l.count - r.count;
+        if (x !== 0) {
+            return x;
+        }
+        return l.uniques - r.uniques;
+    });
+    console.table(list);
+};
+for await (const name of repos()) {
+    const {count, uniques} = await views(name);
+    list.push({name, count, uniques});
+
+    if (isatty) {
+        await tty.clearScreen();
+        await tty.goHome();
+        print();
+    }
+}
+
+if (!isatty) {
+    print();
+}
+
+async function* repos(): AsyncGenerator<string, void, void> {
+    const query = `query($endCursor: String) {
+        viewer {
+            repositories(first: 10, after: $endCursor) {
+                nodes { nameWithOwner }
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+            }
+        }
+    }`;
+
+    const filter = '.data.viewer.repositories.nodes[].nameWithOwner';
+
+    const proc = Deno.run({
+        cmd: ['gh', 'api', 'graphql', '--paginate', '-f', `query=${query}`, '--jq', filter],
+        stdout: 'piped',
+    });
+
+    for await (const line of readLines(proc.stdout)) {
+        yield line;
+    }
+}
+
+async function views(name: string): Promise<any> {
+    const proc = Deno.run({
+        cmd: ['gh', 'api', `repos/${name}/traffic/views`],
+        stdout: 'piped',
+    });
+    const data = [];
+    for await (const line of readLines(proc.stdout)) {
+        data.push(line);
+    }
+
+    const status = await proc.status();
+    if (!status.success) {
+        throw new Error(data.join(""));
+    }
+
+    const c = JSON.parse(data.join(""));
+    return c;
+}
